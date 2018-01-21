f773c904f70b07e18afc59101bd4bf2e963602f0
Younes Peters Touati <younes.touati@citrix.com>
2018-01-21 14:50:06 +0100

    Built storybook as static site in docs folder

---
+++ .storybook/config.js
@@ -0,0 +1,12 @@
+import {configure, addDecorator} from '@storybook/react';
+import {withKnobs} from '@storybook/addon-knobs/react';
+
+const req = require.context('../src', true, /\.stories\.js$/)
+
+function loadStories() {
+  req.keys().forEach((filename) => req(filename))
+}
+
+addDecorator(withKnobs);
+
+configure(loadStories, module);

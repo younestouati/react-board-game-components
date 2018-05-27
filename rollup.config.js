013fa2548c42dbe3d235edc330f03ee9627159bc
Younes Peters Touati <younes.touati@citrix.com>
2018-05-27 10:53:00 +0200

    Updated rollup config and externalised react

--- rollup.config.js
+++ rollup.config.js
@@ -1,49 +1,48 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import {uglify} from 'rollup-plugin-uglify';
+import eslint from 'rollup-plugin-eslint';
 import babel from 'rollup-plugin-babel';
 import image from 'rollup-plugin-image';
+import globals from 'rollup-plugin-node-globals';
 import pkg from './package.json';
 
-export default [
-	// browser-friendly UMD build
-	{
-		external: ['react', 'react-dom'],
-		input: 'src/index.js',
-		output: {
-			name: 'react-board-game-components',
-			file: pkg.browser,
-			format: 'umd'
-		},
-		plugins: [
-			babel({
-				exclude: ['node_modules/**'],
-				plugins: ['external-helpers']
-			}),
-			resolve(),
-			commonjs(),
-			image()
-		]
-	},
+const ENV = process ? process.env.NODE_ENV : JSON.stringify('development');
+const isProduction = ENV === 'production';
+const isDevelopment = ENV === 'development';
 
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify 
-	// `file` and `format` for each target)
-	{
-		input: 'src/index.js',
-		external: ['react', 'react-dom', 'prop-types'],
-		output: [
-			{ dest: pkg.main, format: 'cjs' },
-			{ dest: pkg.module, format: 'es' }
-		],
-		plugins: [
+export default [
+    {
+        input: 'src/index.js',
+        external: ['react', 'react-dom'],
+        globals: {
+            'react': 'React',
+            'react-dom': 'ReactDOM'
+        },
+        output: [
+            {name: 'ReactBoardGameComponents', file: pkg.browser, format: 'umd'}, // browser-friendly UMD build
+            {file: pkg.main, format: 'cjs' }, // CommonJS (for Node)
+            {file: pkg.module, format: 'es' }, // ES module (for bundlers and modern browsers)
+        ],
+        plugins: [
+			/*eslint({
+				exclude: '*.css',
+				envs: ["browser"]
+			}),*/
+			postcss({
+				extensions: ['.css'],
+			}),
 			babel({
 				exclude: ['node_modules/**'],
 				plugins: ['external-helpers']
 			}),
-			image()
-		]
-	}
+			resolve(), // Makes it possible to load third-party modules in node_modules.
+			commonjs(), // Converts CommonJS modules to ES6.
+			image(), // For importing JPG, PNG, GIF and SVG images
+			globals(), // Injects the same node globals browserify does (i.e process, Buffer, etc)    
+			//isProduction && uglify()
+        ],
+        sourcemap: true
+    }
 ];
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Chip from './chip/chip';
 import HexTile from './hex-tile/hex-tile';
 import SquareTile from './square-tile/square-tile';
 import PlayingCard from './playing-card/playing-card';
+import makeStandardDeck from './playing-card/make-standard-deck';
 
 export {
     Chip,
     HexTile,
     SquareTile,
-    PlayingCard
+    PlayingCard,
+    makeStandardDeck
 };
